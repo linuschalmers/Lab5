8b706694f3dc667dd5de4642d99759c8c5834f61
Albin2101 <albin.boklund@gmail.com>
2021-10-19 10:45:08 +0200

    Update Famous.hs

--- Famous.hs
+++ Famous.hs
@@ -1,8 +1,16 @@
-
-
 data QA = Q String QA QA | Ans String
     deriving (Show, Read)
 
+que :: String
+que = "Is she from Europe"
+
+que1 :: String
+que1 = "Is she a scientist?"
+
+que2 :: String
+que2 = "Is sha an actress?"
+
+tree = 
 
 question :: String -> IO String
 question que = do
@@ -11,10 +19,17 @@ question que = do
 
 
 yesNoQ :: String -> IO Bool
-yesNoQ que = do 
-    if head que ==  'y' then return True
+yesNoQ que = do
+    ans <- question que 
+    if head ans ==  'y' then return True
     else return False
-    
+
+play :: QA -> IO QA
+play (Q que que1 que2) = do
+    ans <- yesNoQ que
+    if ans == True then que1
+    else que2    
+
 
 
 
