fbd9d1f9b97848e50db9f8b52f15c161c6acca05
linuschalmers <linuschalmers@gmail.com>
2021-10-19 11:46:15 +0200

    Update Famous.hs

--- Famous.hs
+++ Famous.hs
@@ -1,3 +1,4 @@
+
 data QA = Q String QA QA | Ans String
     deriving (Show, Read)
 
@@ -10,26 +11,24 @@ que1 = "Is she a scientist?"
 que2 :: String
 que2 = "Is sha an actress?"
 
-tree = 
+tree = Q "Is she from Europe?" (Q "Is she a scientist?" (Ans ) (Ans)) (Q "Is sha an actress?" (Ans) (Ans))
 
 question :: String -> IO String
 question que = do
     putStr que
     getLine
 
-
 yesNoQ :: String -> IO Bool
 yesNoQ que = do
     ans <- question que 
-    if head ans ==  'y' then return True
+    if head ans == 'y' then return True
     else return False
-
+    
 play :: QA -> IO QA
 play (Q que que1 que2) = do
     ans <- yesNoQ que
     if ans == True then que1
     else que2    
-
 
 
 
